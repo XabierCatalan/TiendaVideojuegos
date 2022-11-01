a4de0806c838e8a93c69e99b03690c3e26c17f11
XabierCatalan <xabier.catalan@opendeusto.es>
2022-11-01 19:48:31 +0100

    creacion enum Genero y cambios clase videojuegos

--- TiendaVideojuegos/src/ClasesPrincipales/Videojuego.java
+++ TiendaVideojuegos/src/ClasesPrincipales/Videojuego.java
@@ -1,6 +1,17 @@
 package ClasesPrincipales;
 
-public class Videojuego {
-	protected String nombre;
-	protected int id;
+import java.io.Serializable;
+
+public class Videojuego extends Producto implements Serializable,Pagable {
+	
+	protected int año;
+	protected double precio;
+	
+
+	@Override
+	public double getPrecio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
