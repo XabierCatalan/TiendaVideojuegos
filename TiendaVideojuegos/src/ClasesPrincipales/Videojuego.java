815cfbd820fdda04e52784d931fd93966d6b87f3
XabierCatalan <xabier.catalan@opendeusto.es>
2022-11-01 21:23:51 +0100

    cambio metodo getPrecio clase Videojuego

--- TiendaVideojuegos/src/ClasesPrincipales/Videojuego.java
+++ TiendaVideojuegos/src/ClasesPrincipales/Videojuego.java
@@ -62,19 +62,23 @@ public class Videojuego extends Producto implements Serializable,Pagable {
 	}
 	
 	@Override
-	public String toString() {
-		return nombre + "(" + anyo +") (" + genero + ") (" + estado + ")" + precio + "eur";
-	}
-
-	@Override
 	public double getPrecio() {
-	if(this.getEstado()== estado.SEGUNDA_MANO) {
+	if(this.getEstado()== EstadoProducto.SEGUNDA_MANO) {
 		return precio / 3;
 		
 	}else {
 		return precio;
 	}
 }
+	
+	
+	@Override
+	public String toString() {
+		return nombre + "(" + anyo +") (" + genero + ") (" + estado + ")" + precio + "eur";
+	}
+
+	
+
 
 	
 		
