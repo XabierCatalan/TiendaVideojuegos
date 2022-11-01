98299590426e664f7df0bed21f251f68f7a3d6e6
XabierCatalan <xabier.catalan@opendeusto.es>
2022-11-01 19:39:15 +0100

    cambio a la clase Producto

--- TiendaVideojuegos/src/ClasesPrincipales/Producto.java
+++ TiendaVideojuegos/src/ClasesPrincipales/Producto.java
@@ -1,5 +1,47 @@
 package ClasesPrincipales;
 
 public class Producto {
+	protected int id;
 	protected String nombre;
+	
+	public Producto(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	public Producto() {
+		super();
+		this.id = 0;
+		this.nombre = "Sin nombre";
+	}
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto" + nombre + "(id: " + id + ")";
+	}
+	
+	
+	
+	
+	
+	
 }
