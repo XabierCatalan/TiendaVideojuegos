017401fe2aec9314fefbb898df3d1da8c313a27b
XabierCatalan <xabier.catalan@opendeusto.es>
2022-11-01 20:32:35 +0100

    actualizacion clase Videojuego

--- TiendaVideojuegos/src/ClasesPrincipales/Videojuego.java
+++ TiendaVideojuegos/src/ClasesPrincipales/Videojuego.java
@@ -8,11 +8,66 @@ public class Videojuego extends Producto implements Serializable,Pagable {
 	protected int año;
 	protected double precio;
 	
+	
+	
+
+	public Videojuego(int id, String nombre, Genero genero, EstadoProducto estado, int año, double precio) {
+		super(id, nombre);
+		this.genero = genero;
+		this.estado = estado;
+		this.año = año;
+		this.precio = precio;
+	}
+	
+	public Videojuego() {
+		super();
+		this.genero = genero;
+		this.estado = estado;
+		this.año = año;
+		this.precio = precio;
+	}
+	
+	
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+	public EstadoProducto getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoProducto estado) {
+		this.estado = estado;
+	}
+
+	public int getAño() {
+		return año;
+	}
+
+	public void setAño(int año) {
+		this.año = año;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
 
 	@Override
 	public double getPrecio() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+	@Override
+	public String toString() {
+		return nombre + "(" + genero + ") (" + estado + ")" + precio + "eur";
+	}
+	
+	
 	
 }
