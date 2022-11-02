90adca945b539459da4bb230c5890c0baab230b0
XabierCatalan <xabier.catalan@opendeusto.es>
2022-11-02 10:43:28 +0100

    creacion bien hecha Test Producto

---
+++ TiendaVideojuegos/Test/ClasesPrincipales/TestProducto.java
@@ -0,0 +1,44 @@
+package ClasesPrincipales;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestProducto {
+
+	@Test
+	public void testProductoIntString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProducto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
