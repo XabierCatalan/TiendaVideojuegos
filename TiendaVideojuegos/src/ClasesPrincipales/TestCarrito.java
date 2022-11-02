215c3855df2b4089b4f0354fffd300f47fe8b4f6
XabierCatalan <xabier.catalan@opendeusto.es>
2022-11-02 12:19:23 +0100

    creacion TestCarrito

---
+++ TiendaVideojuegos/src/ClasesPrincipales/TestCarrito.java
@@ -0,0 +1,69 @@
+package ClasesPrincipales;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCarrito {
+
+	@Test
+	public void testCarritoStringDateEstadoCarrito() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCarrito() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCliente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCliente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFecha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFecha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEstadoCarrito() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEstadoCarrito() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetElementos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetElementos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrecio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
