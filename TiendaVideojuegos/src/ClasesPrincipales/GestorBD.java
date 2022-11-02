2df591a0c89b61558bf6542b211361e8f56f51e2
Oscar Perez <o.perez@opendeusto.es>
2022-11-02 18:11:17 +0100

    BASE DE DATOS

--- TiendaVideojuegos/src/ClasesPrincipales/GestorBD.java
+++ TiendaVideojuegos/src/ClasesPrincipales/GestorBD.java
@@ -1,8 +1,37 @@
 package ClasesPrincipales;
 
+
 import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 
 public class GestorBD {
 	protected static final String DRIVER_NAME = "org.sqlite.JDBC";
+	protected static final String DATABASE_FILE = "db/database.db";
+	protected static final String CONNECTION_STRING = "jdbc:sqlite:" + DATABASE_FILE;
+	
+	public GestorBD() {
+		try {
+			Class.forName(DRIVER_NAME);
+		} catch (ClassNotFoundException ex) {
+			System.err.println(String.format("* Error al cargar el driver de BBDD: %s", ex.getMessage()));
+			ex.printStackTrace();
+		}
+	}
+	
+	public void CrearBBDD() {
+		try (Connection con = DriverManager.getConnection(CONNECTION_STRING);
+			 Statement stmt = con.createStatement()) {
+			
+			String sql = "CREATE TABLE IF NOT EXISTS CLIENTE (\n"
+					+ " ID INTEGER PRIMARY KEY AUTOINCREMENT, \n"
+					+ " NAME TEXT NOT NULL,\n"
+					+ 
+			
+		}
+	}
+	
 
 }
