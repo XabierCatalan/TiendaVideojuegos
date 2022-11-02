459a7cc7f8e38b9b4b04f446755ac18864de167d
XabierCatalan <xabier.catalan@opendeusto.es>
2022-11-02 10:35:45 +0100

    creacion del primer JUnit para la clase producto

---
+++ TiendaVideojuegos/Test/ClasesPrincipales/ProductoTest.java
@@ -0,0 +1,14 @@
+package ClasesPrincipales;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProductoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
