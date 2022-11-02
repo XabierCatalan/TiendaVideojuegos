a28d1627b2de16b19e2493466362969d991fc9ff
XabierCatalan <xabier.catalan@opendeusto.es>
2022-11-02 18:50:19 +0100

    actualizacion GestorBD

--- TiendaVideojuegos/src/ClasesPrincipales/GestorBD.java
+++ TiendaVideojuegos/src/ClasesPrincipales/GestorBD.java
@@ -2,6 +2,7 @@ package ClasesPrincipales;
 
 
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -28,8 +29,44 @@ public class GestorBD {
 			String sql = "CREATE TABLE IF NOT EXISTS CLIENTE (\n"
 					+ " ID INTEGER PRIMARY KEY AUTOINCREMENT, \n"
 					+ " NAME TEXT NOT NULL,\n"
-					+ 
+					+ " GENERO ENUM, \n"
+					+ "ESTADOPRODUCTO ENUM,\n"
+					+ "ANYO INTEGER,\n"
+					+ "PRECIO DOUBLE,\n"
+					+ "MARCA ENUM,\n"
+					+ "ESTADOPRODUCTO ENUM,\n"
+					+ ");";
+					
 			
+		}catch (Exception ex) {
+			System.err.println(String.format("* Error al crear la BBDD: %s", ex.getMessage()));
+			ex.printStackTrace();
+		}
+	}
+	
+	public void borrarBBDD() {
+		//Se abre la conexión y se obtiene el Statement
+		try (Connection con = DriverManager.getConnection(CONNECTION_STRING);
+		     Statement stmt = con.createStatement()) {
+			
+	        String sql = "DROP TABLE IF EXISTS CLIENTE";
+			
+	        //Se ejecuta la sentencia de creación de la tabla Estudiantes
+	        if (!stmt.execute(sql)) {
+	        	System.out.println("- Se ha borrado la tabla Cliente");
+	        }
+		} catch (Exception ex) {
+			System.err.println(String.format("* Error al borrar la BBDD: %s", ex.getMessage()));
+			ex.printStackTrace();			
+		}
+		
+		try {
+			//Se borra el fichero de la BBDD
+			Files.delete(Paths.get(DATABASE_FILE));
+			System.out.println("- Se ha borrado el fichero de la BBDD");
+		} catch (Exception ex) {
+			System.err.println(String.format("* Error al borrar el archivo de la BBDD: %s", ex.getMessage()));
+			ex.printStackTrace();						
 		}
 	}
 	
