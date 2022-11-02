c64a8ba57accf649239e22d445fc61d755c885b0
enekuri08 <enekode.miguel@opendeusto.es>
2022-11-02 14:33:01 +0100

    j unit test consola

--- TiendaVideojuegos/test/ClasesPrincipales/TestConsola.java
+++ TiendaVideojuegos/test/ClasesPrincipales/TestConsola.java
@@ -2,53 +2,87 @@ package ClasesPrincipales;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestConsola {
-
+	Consola consola;
+	protected int id = 0;
+	protected String nombre = "nombre";
+	protected double precio=0;
+	protected EstadoProducto estado = EstadoProducto.PRIMERA_MANO;
+	protected Marca marca = Marca.NINTENDO;
+	
+	@Before
+	public void SetUp() {
+	consola = new Consola();
+	consola.setNombre(nombre);
+	consola.setId(id);
+	consola.setEstado(estado);
+	consola.setPrecio(precio);
+	consola.setMarca(marca);
+	}
+	
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		String toString = nombre + "(" + estado + ") ("+ marca + ")" + precio + "eur";
 	}
 
 	@Test
 	public void testConsolaIntStringDoubleEstadoProductoMarca() {
-		fail("Not yet implemented");
-	}
+		Consola newConsola = new Consola(id,nombre,precio,estado,marca);
+		assertNotNull(newConsola);
+		assertEquals(newConsola.getNombre(), nombre);
+		assertEquals(newConsola.getId(), id,0);
+		assertEquals(newConsola.getEstado(), estado);
+		assertEquals(newConsola.getPrecio(), precio,0);
+		assertEquals(newConsola.getMarca(), marca);
+				}
 
 	@Test
 	public void testConsola() {
-		fail("Not yet implemented");
-	}
+		Consola newConsola2 = new Consola();
+		assertNotNull(newConsola2);
+		assertEquals(newConsola2.getNombre(), "Sin nombre");
+		assertEquals(newConsola2.getId(), 0,0);
+		assertEquals(newConsola2.getEstado(), estado.PRIMERA_MANO);
+		assertEquals(newConsola2.getPrecio(), 0,0);
+		assertEquals(newConsola2.getMarca(), marca.PLAYSTATION);	}
 
 	@Test
 	public void testGetPrecio() {
-		fail("Not yet implemented");
+		assertEquals(consola.getPrecio(), precio,0);
 	}
 
 	@Test
 	public void testSetPrecio() {
-		fail("Not yet implemented");
-	}
+		Double newPrecio = 2.0;
+		assertEquals(consola.getPrecio(), precio,0);
+		consola.setPrecio(newPrecio);
+		assertEquals(consola.getPrecio(), newPrecio,0);	}
 
 	@Test
 	public void testGetEstado() {
-		fail("Not yet implemented");
+		assertEquals(consola.getEstado(), estado);
 	}
 
 	@Test
 	public void testSetEstado() {
-		fail("Not yet implemented");
-	}
+		EstadoProducto newEstado = EstadoProducto.SEGUNDA_MANO;
+		assertEquals(consola.getEstado(), estado);
+		consola.setEstado(newEstado);
+		assertEquals(consola.getEstado(), newEstado);	}
 
 	@Test
 	public void testGetMarca() {
-		fail("Not yet implemented");
+		assertEquals(consola.getMarca(), marca);
 	}
 
 	@Test
 	public void testSetMarca() {
-		fail("Not yet implemented");
-	}
+		Marca newMarca = Marca.PLAYSTATION;
+		assertEquals(consola.getMarca(), marca);
+		consola.setMarca(newMarca);
+		assertEquals(consola.getMarca(), newMarca);	}
 
 }
