fcbe53975e7c1f386965e3a2d2cf0ff485329a97
XabierCatalan <xabier.catalan@opendeusto.es>
2022-11-02 18:12:02 +0100

    creacion nuevo TestServicio

---
+++ TiendaVideojuegos/test/ClasesPrincipales/TestServicio.java
@@ -0,0 +1,49 @@
+package ClasesPrincipales;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestServicio {
+
+	@Test
+	public void testServicioTipoServicioDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testServicio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTipo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTipo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFecha1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFecha1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrecio() {
+		fail("Not yet implemented");
+	}
+
+}
