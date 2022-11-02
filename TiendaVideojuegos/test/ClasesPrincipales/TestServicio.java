ceee1cdca46efe061db182d2966d21ed929d2c4c
Oscar Perez <o.perez@opendeusto.es>
2022-11-02 19:08:15 +0100

    TestServicio

--- TiendaVideojuegos/test/ClasesPrincipales/TestServicio.java
+++ TiendaVideojuegos/test/ClasesPrincipales/TestServicio.java
@@ -2,43 +2,74 @@ package ClasesPrincipales;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestServicio {
+	
+	
+	
+	Servicio servicio;
+	protected TipoServicio tipo = TipoServicio.MANTENIMIENTO;
+	protected Date fecha = new Date();
+	
+	
+	
+	public void SetUp() {
+		servicio = new Servicio();
+		servicio.setTipo(tipo);
+		servicio.setTipo(tipo);
+	}
 
 	@Test
 	public void testServicioTipoServicioDate() {
-		fail("Not yet implemented");
+		Servicio newServicio = new Servicio(tipo,fecha);
+		assertNotNull(newServicio);
+		assertEquals(newServicio.getTipo(), tipo);
+		assertEquals(newServicio.getFecha1(), fecha);
 	}
 
 	@Test
 	public void testServicio() {
-		fail("Not yet implemented");
+		Servicio newServicio2 = new Servicio();
+		assertNotNull(newServicio2);
+		assertEquals(newServicio2.getTipo(), TipoServicio.MANTENIMIENTO);
+		assertEquals(newServicio2.getFecha1(), new Date());
 	}
 
 	@Test
 	public void testGetTipo() {
-		fail("Not yet implemented");
+		assertEquals(servicio.getTipo(), tipo);
 	}
 
 	@Test
 	public void testSetTipo() {
-		fail("Not yet implemented");
+		TipoServicio newTipo = TipoServicio.REPARACION;
+		assertEquals(servicio.getTipo(),tipo);
+		servicio.setTipo(newTipo);
+		assertEquals(servicio.getTipo(), newTipo);
 	}
 
 	@Test
 	public void testGetFecha1() {
-		fail("Not yet implemented");
+		assertEquals(servicio.getFecha1(), fecha);
 	}
 
 	@Test
 	public void testSetFecha1() {
-		fail("Not yet implemented");
+		Date newDate = new Date();
+		assertEquals(servicio.getFecha1(), fecha);
+		servicio.setFecha1(newDate);
+		assertEquals(servicio.getFecha1(), newDate);
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		
+		
+		
 	}
 
 	@Test
