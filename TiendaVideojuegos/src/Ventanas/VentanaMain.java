b717b1d0462fe585dc584cebc614cc6418c5ec0f
XabierCatalan <xabier.catalan@opendeusto.es>
2022-12-01 18:21:25 +0100

    cambios en la VentanaMain

--- TiendaVideojuegos/src/Ventanas/VentanaMain.java
+++ TiendaVideojuegos/src/Ventanas/VentanaMain.java
@@ -19,8 +19,41 @@ public class VentanaMain extends JFrame{
 		
 		Container cp = this.getContentPane();
 		
-		cp.add(new JLabel(new ImageIcon("deustea.png")), BorderLayout.CENTER);
+		cp.add(new JLabel(new ImageIcon("game.png")), BorderLayout.CENTER);
 		
+		JPanel abajo = new JPanel();
+		
+		botonIniciarSesion = new JButton("Iniciar Sesion");
+		botonCrearCuenta = new JButton("Crear Nueva Cuenta");
+		
+		botonIniciarSesion.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				ventanaIniciarSesion.setVisible(true);
+			}
+		});
+		
+		botonCrearCuenta.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				ventanaCrearCuenta.setVisible(true);
+			}
+		});
+		
+		abajo.add(botonIniciarSesion);
+		abajo.add(botonCrearCuenta);
+		
+		cp.add(abajo, BorderLayout.SOUTH);
+		
+		this.setTitle("TiendaGame");
+		this.pack();
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+	
+	public static void main(String[] args) {
+		VentanaMain v = new VentanaMain();
+		v.setVisible(true);
 	}
 
 }
