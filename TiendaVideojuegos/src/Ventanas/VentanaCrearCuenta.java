e5dabe4bec0c6a9726e0e315493ad0576bea46a6
XabierCatalan <xabier.catalan@opendeusto.es>
2022-12-01 18:44:03 +0100

    cambios en VentanaCrearCuenta

--- TiendaVideojuegos/src/Ventanas/VentanaCrearCuenta.java
+++ TiendaVideojuegos/src/Ventanas/VentanaCrearCuenta.java
@@ -1,5 +1,20 @@
 package Ventanas;
 
-public class VentanaCrearCuenta {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import ClasesPrincipales.*;
+
+public class VentanaCrearCuenta extends JFrame {
+	
+	
+	public VentanaCrearCuenta() {
+		
+		this.setTitle("CrearCuenta");
+		this.setSize(600, 600);
+		this.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		this.setVisible(true);
+	}
 
 }
