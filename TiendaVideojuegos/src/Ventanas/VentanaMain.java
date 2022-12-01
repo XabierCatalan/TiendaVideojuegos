9933195774c58e451255925f086e4375edde4ba2
XabierCatalan <xabier.catalan@opendeusto.es>
2022-12-01 18:03:43 +0100

    foto de GAME y cambios en VentanaMain

--- TiendaVideojuegos/src/Ventanas/VentanaMain.java
+++ TiendaVideojuegos/src/Ventanas/VentanaMain.java
@@ -6,10 +6,21 @@ import javax.swing.*;
 
 import ClasesPrincipales.*;
 
-public class VentanaMain {
+public class VentanaMain extends JFrame{
 	protected JButton botonIniciarSesion;
 	protected JButton botonCrearCuenta;
 	protected VentanaInicioSesion ventanaInicioSesion;
 	protected VentanaCrearCuenta ventanaCrearCuenta;
+	
+	public VentanaMain() {
+		ventanaInicioSesion = new VentanaInicioSesion();
+		ventanaCrearCuenta = new VentanaCrearCuenta	();
+		
+		
+		Container cp = this.getContentPane();
+		
+		cp.add(new JLabel(new ImageIcon("deustea.png")), BorderLayout.CENTER);
+		
+	}
 
 }
