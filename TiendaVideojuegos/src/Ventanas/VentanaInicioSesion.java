f26cee839ac9c5a9e5f29783c4a9da227a9f28b9
Oscar Perez <o.perez@opendeusto.es>
2022-12-01 18:17:16 +0100

    parte ventana inicio sesion

--- TiendaVideojuegos/src/Ventanas/VentanaInicioSesion.java
+++ TiendaVideojuegos/src/Ventanas/VentanaInicioSesion.java
@@ -1,5 +1,29 @@
 package Ventanas;
 
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class VentanaInicioSesion {
+	
+	protected JButton Atras;
+	protected JButton iniciarSesion;
+	protected JLabel Mail;
+	protected JLabel Contraseña;
+	protected JTextField mail1;
+	protected JTextField contraseña;
+	protected JLabel IntroDatos;
+	
+	public VentanaInicioSesion() {
+		
+		Container cp = this.
+		
+	}
+	
+
+	
+
+	
 
 }
