7949d4cbc5e5df56fe73c54e7255e58e73c900d9
XabierCatalan <xabier.catalan@opendeusto.es>
2022-12-01 17:25:31 +0100

    cambios en clase VentanaMain y creacion de las clases VentanaInicioSesion Y VentanaCrearCuenta

--- TiendaVideojuegos/src/Ventanas/VentanaMain.java
+++ TiendaVideojuegos/src/Ventanas/VentanaMain.java
@@ -7,5 +7,9 @@ import javax.swing.*;
 import ClasesPrincipales.*;
 
 public class VentanaMain {
+	protected JButton botonIniciarSesion;
+	protected JButton botonCrearCuenta;
+	protected VentanaInicioSesion ventanaInicioSesion;
+	protected VentanaCrearCuenta ventanaCrearCuenta;
 
 }
