993539ac0d03b56fb9abb7470be5b190fb2d5e4d
Oscar Perez <o.perez@opendeusto.es>
2022-12-01 19:17:54 +0100

    iniciar sesion v5

--- TiendaVideojuegos/src/Ventanas/VentanaInicioSesion.java
+++ TiendaVideojuegos/src/Ventanas/VentanaInicioSesion.java
@@ -36,43 +36,38 @@ public class VentanaInicioSesion extends JFrame {
 		JP1.setLayout(new FlowLayout()); //boton Atras
 		
 		JPanel JP2 = new JPanel(); //IntroDatos , Mail , mail1, Contraseña, contraseña1
-		JP2.setLayout(new GridLayout(2,1));
+		JP2.setLayout(new GridLayout(2,2));
 		
-		JPanel JP3 = new JPanel(); //Mail , mail1, Contraseña, contraseña1
-		JP3.setLayout(new GridLayout(2,2));
 		
 		JPanel JP4 = new JPanel(); //InicioSesion
 		JP4.setLayout(new FlowLayout());
 		
-		JPanel JP5 = new JPanel(); //relleno
-		JPanel JP6 = new JPanel(); // relleno
+		
 		
 		cp.setLayout(BL1);
 		
-		add(JP1, BL1.NORTH); //boton Atras
-		JP1.add(Atras); //boton Atras
+		add(JP1, BL1.NORTH); //IntroDatos
+		JP1.add(IntroDatos); //IntroDatos
 		
-		add(JP2, BL1.CENTER); //IntroDatos , Mail , mail1, Contraseña, Contraseña1
-		JP2.add(IntroDatos); //IntroDatos
-		JP3.add(Mail); //Mail
-		JP3.add(mail1); //mail1
-		JP3.add(Contraseña); //Contraseña
-		JP3.add(contraseña1); //Contraseña1
-		JP2.add(JP3);
+		add(JP2, BL1.CENTER);  // Mail , mail1, Contraseña, Contraseña1
+		
+		JP2.add(Mail); //Mail
+		JP2.add(mail1); //mail1
+		JP2.add(Contraseña); //Contraseña
+		JP2.add(contraseña1); //Contraseña1
+		
 		
 		add(JP4, BL1.SOUTH);
+		JP4.add(Atras);
 		JP4.add(iniciarSesion);
-		
-		add(JP5, BL1.EAST);
-		add(JP5, BL1.WEST);
 		
 		
 		
 		
 		this.setTitle("Ventana Inicio Sesion");
-		this.setResizable(true);
-		this.setSize(600,600);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setSize(300,300);
+		this.setDefaultCloseOperation(HIDE_ON_CLOSE);
 		this.setVisible(false);
 	}
 	
