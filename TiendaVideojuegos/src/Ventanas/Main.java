a8c322f2d6079702ed6d0cb5913437490476d983
XabierCatalan <xabier.catalan@opendeusto.es>
2022-12-08 19:27:21 +0100

    creacion VentanaConsolaMandos y cambios en Main

--- TiendaVideojuegos/src/Ventanas/Main.java
+++ TiendaVideojuegos/src/Ventanas/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	static VentanaInicioSesion vIS = new VentanaInicioSesion();
 	static VentanaCrearCuenta vCC = new VentanaCrearCuenta();
 	static VentanaMenuPrincipal vMP = new VentanaMenuPrincipal();
+	static VentanaConsolaMandos vCM = new VentanaConsolaMandos();
 	
 	public static void main(String[] args) {
 		vM.setVisible(true);
