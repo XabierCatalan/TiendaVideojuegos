7aba9d3da79511c10d4f3a1d9e4f4d07a67d8e03
UnaiiGonzalezz <unai.gonzalez@opendeusto.es>
2022-12-08 18:26:28 +0100

    VentanaMain: imagen redimensionada, boton entrar tienda incluido

--- TiendaVideojuegos/src/Ventanas/VentanaMain.java
+++ TiendaVideojuegos/src/Ventanas/VentanaMain.java
@@ -11,22 +11,26 @@ import ClasesPrincipales.*;
 public class VentanaMain extends JFrame{
 	protected JButton botonIniciarSesion;
 	protected JButton botonCrearCuenta;
+	protected JButton botonEntrarTienda;
 	
 	
 	
 	public VentanaMain() {
 		
 		
-		
-		
 		Container cp = this.getContentPane();
-		
-		cp.add(new JLabel(new ImageIcon("game.png")), BorderLayout.CENTER);
+		ImageIcon imageIcon = new ImageIcon("game.png");
+		Image image = new ImageIcon("game.png").getImage();
+		Image newImg = image.getScaledInstance(500, 215, java.awt.Image.SCALE_SMOOTH);
+		imageIcon = new ImageIcon(newImg);
+		cp.add(new JLabel(imageIcon), BorderLayout.NORTH);
 		
 		JPanel abajo = new JPanel();
+		JPanel abajo2 = new JPanel();
 		
 		botonIniciarSesion = new JButton("Iniciar Sesion");
 		botonCrearCuenta = new JButton("Crear Nueva Cuenta");
+		botonEntrarTienda = new JButton("Entar en la tienda");
 		
 		botonIniciarSesion.addActionListener(new ActionListener() {
 			@Override
@@ -42,14 +46,23 @@ public class VentanaMain extends JFrame{
 			public void actionPerformed(ActionEvent arg0) {
 				Main.vCC.setVisible(true);
 				VentanaMain.this.setVisible(false);
-				
+			}
+		});
+		
+		botonEntrarTienda.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Main.vMP.setVisible(true);
+				VentanaMain.this.setVisible(false);
 			}
 		});
 		
 		abajo.add(botonIniciarSesion);
 		abajo.add(botonCrearCuenta);
+		abajo2.add(botonEntrarTienda);
 		
-		cp.add(abajo, BorderLayout.SOUTH);
+		cp.add(abajo, BorderLayout.CENTER);
+		cp.add(abajo2, BorderLayout.SOUTH);
 		
 		this.setTitle("TiendaGame");
 		this.pack();
