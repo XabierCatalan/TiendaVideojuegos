565c7aed269a35d92b342aabc38d7a9e3c883a0c
XabierCatalan <xabier.catalan@opendeusto.es>
2022-12-09 19:06:24 +0100

    cambios en VentanaConsolaMandos

--- TiendaVideojuegos/src/Ventanas/VentanaConsolaMandos.java
+++ TiendaVideojuegos/src/Ventanas/VentanaConsolaMandos.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.util.List;
 
 import ClasesPrincipales.*;
 
@@ -13,10 +14,11 @@ public class VentanaConsolaMandos extends JFrame {
 			new Object[] { "Nombre", "Marca", "Estado" ,"Precio" }, 0
 			);
 	
-	protected JTable tCM = new JTable ();
+	protected JTable tCM = new JTable (mCM);
 	
-	//protected List<Consola> listaConsolas;
-	//protected List<Mando> listaMandos;
+	protected List<Consola> listaConsola;
+	protected List<Mando> listaMando;
+	
 	
 	
 	public VentanaConsolaMandos () {
@@ -28,6 +30,21 @@ public class VentanaConsolaMandos extends JFrame {
 		JPanel pIzquierda = new JPanel();
 		pIzquierda.add( new JScrollPane( tCM ));
 		getContentPane().add( pIzquierda, BorderLayout.WEST );
+	}
+	
+	
+	private void cargarTabla() {
+		listaConsola = Main.bd.obtenerDatosConsolas();
+		listaMando = Main.bd.obtenerDatosMandos();
+		
+		for (Consola consola : listaConsola) {
+			mCM.addRow( new Object[] { consola.getNombre(), consola.getMarca(), consola.getEstado(), consola.getPrecio() } );
+		}
+		for (Mando mando : listaMando) {
+			mCM.addRow( new Object[] { mando.getNombre(), mando.getMarca(), mando.getEstado(), mando.getPrecio() } );
+		}
+		
+		
 	}
 
 }
