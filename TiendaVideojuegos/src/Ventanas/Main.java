a33eb2df90635b388db8cc0a72fa75aaf938b387
UnaiiGonzalezz <unai.gonzalez@opendeusto.es>
2022-12-10 14:50:16 +0100

    Logger creado

--- TiendaVideojuegos/src/Ventanas/Main.java
+++ TiendaVideojuegos/src/Ventanas/Main.java
@@ -2,6 +2,7 @@ package Ventanas;
 
 import ClasesPrincipales.GestorBD;
 import ClasesPrincipales.TiendaGame;
+import java.util.logging.Logger;
 
 public class Main {
 
@@ -15,8 +16,23 @@ public class Main {
 	static VentanaVideojuegos vV = new VentanaVideojuegos();
 	static VentanaCarrito vC = new VentanaCarrito();
 	static VentanaServicio vS = new VentanaServicio();
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+    
+    /*
+     * NIVELES LOGGER
+     * OFF		0	No logging
+	 * FATAL	100	The application is unusable. Action needs to be taken immediately.
+	 * ERROR	200	An error occurred in the application.
+     * WARN		300	Something unexpected—though not necessarily an error—happened and needs to be watched.
+     * INFO		400	A normal, expected, relevant event happened.
+     * DEBUG	500	Used for debugging purposes
+     * TRACE	600	Used for debugging purposes—includes the most detailed information
+     */
+
 	
 	public static void main(String[] args) {
+		
+		LOG.info("Programa iniciado");		
 		vM.setVisible(true);
 		bd.borrarBBDDCarrito();
 		bd.borrarBBDDConsola();
