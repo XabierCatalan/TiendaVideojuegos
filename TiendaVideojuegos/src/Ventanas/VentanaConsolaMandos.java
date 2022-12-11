5937be2465f6e4ef106ad7083e89f9fa28dee80e
XabierCatalan <xabier.catalan@opendeusto.es>
2022-12-11 12:52:27 +0100

    cambios en VentanaConsolaMandos

--- TiendaVideojuegos/src/Ventanas/VentanaConsolaMandos.java
+++ TiendaVideojuegos/src/Ventanas/VentanaConsolaMandos.java
@@ -9,6 +9,17 @@ import java.util.List;
 import ClasesPrincipales.*;
 
 public class VentanaConsolaMandos extends JFrame {
+	protected JButton botonCarrito;
+	protected JButton botonAtras;
+	protected JButton botonAnyadirCarrito;
+	protected JButton botonFiltrar;
+	protected JButton botonSinFiltros;
+	
+	
+	protected JLabel labelFiltros;
+	
+	protected JComboBox<EstadoProducto> comboEstado;
+	protected JComboBox<Marca> comboMarca;
 	
 	protected DefaultTableModel mCM = new DefaultTableModel (
 			new Object[] { "Nombre", "Marca", "Estado" ,"Precio" }, 0
@@ -23,8 +34,25 @@ public class VentanaConsolaMandos extends JFrame {
 	
 	
 	public VentanaConsolaMandos () {
+		
+		Container cp = this.getContentPane();
+		
+		cp.setLayout(new FlowLayout());
+		
+		botonAnyadirCarrito = new JButton("Añadir al carrito");
+		botonAtras = new JButton("Atras");
+		botonCarrito = new JButton("Carrito");
+		botonFiltrar = new JButton("Filtrar");
+		botonSinFiltros = new JButton("Quitar Filtros");
+		
+		
+		labelFiltros = new JLabel("FILTROS");
+		
+		comboEstado = new JComboBox<>(EstadoProducto.values());
+		comboMarca = new JComboBox<>(Marca.values());
+		
 		this.setTitle("Consolas y Mandos");
-		this.setSize(1200, 800);
+		this.setSize(800, 400);
 		this.setDefaultCloseOperation(HIDE_ON_CLOSE);
 		this.setVisible(false);
 		
@@ -32,7 +60,37 @@ public class VentanaConsolaMandos extends JFrame {
 		
 		JPanel pIzquierda = new JPanel();
 		pIzquierda.add( new JScrollPane( tCM ));
-		getContentPane().add( pIzquierda, BorderLayout.WEST );
+		cp.add( pIzquierda, BorderLayout.WEST );
+		
+		
+		
+		
+		JPanel pFiltros = new JPanel();
+		pFiltros.setLayout(new GridLayout(6,1));
+		
+		JPanel pBotones = new JPanel();
+		pBotones.setLayout(new BorderLayout());
+		
+		pFiltros.add(labelFiltros);
+		pFiltros.add(comboMarca);
+		pFiltros.add(comboEstado);
+		pFiltros.add(botonFiltrar);
+		pFiltros.add(botonSinFiltros);
+		
+		
+		cp.add(pFiltros);
+		
+		pBotones.add(botonCarrito, BorderLayout.NORTH);
+		pBotones.add(botonAnyadirCarrito, BorderLayout.CENTER);
+		pBotones.add(botonAtras, BorderLayout.SOUTH);
+		
+		cp.add(pBotones);
+		
+		
+		
+		
+		
+		
 	}
 	
 	
