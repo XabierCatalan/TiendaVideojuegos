e4dc238a2d4d3c5602cbea89936db9ce2bc8680c
Oscar Perez <o.perez@opendeusto.es>
2023-01-04 18:06:48 +0100

    CARRITO VBETA

--- TiendaVideojuegos/src/Ventanas/VentanaCarrito.java
+++ TiendaVideojuegos/src/Ventanas/VentanaCarrito.java
@@ -13,7 +13,7 @@ public class VentanaCarrito extends JFrame{
 	protected JButton BorrarDelCarro;
 	protected JButton ConfirmarPedido;
 	
-	protected JLabel CARRITO;
+	
 	protected JLabel precio;
 	
 	protected JTextField precio2;
@@ -32,46 +32,57 @@ public class VentanaCarrito extends JFrame{
 		BorrarDelCarro = new JButton("BORRAR DEL CARRITO");
 		ConfirmarPedido = new JButton("CONFIRMAR PEDIDO");
 		
-		CARRITO = new JLabel("CARRITO");
+		
 		precio = new JLabel("PRECIO:");
 		
 		precio2 = new JTextField("");
 		
 		DLM = new DefaultListModel<>();
 		
+		DLM.add(0, new Mando());;
+		
 		pagables = new JList<>(DLM);
 		
+		JScrollPane ScrollPagables = new JScrollPane(pagables);
+		
+		//PANEL PRINCIPAL JP1
 		JPanel JP1 = new JPanel();
 		JP1.setLayout(new BorderLayout());
 		
+		//PARTE DE ARRIBA DEL PANEL PRINCIPAL JP2
 		JPanel JP2 = new JPanel();
 		JP2.setLayout(new BorderLayout());
 		
 		JP2.add(Atras , BorderLayout.WEST);
-		JP2.add(CARRITO , BorderLayout.CENTER);
+		
 		JP1.add(JP2 , BorderLayout.NORTH);
 		
+		//PANEL DE EL CENTRO DEL PANEL PRINCIPAL JP3
 		JPanel JP3 = new JPanel();
-		JP3.setLayout(new FlowLayout());
+		JP3.setLayout(new BorderLayout());
 		
+		// PANEL DE EL CENTRO DEL PANEL DEL CENTRO DEL PANEL PRINCIPAL JP4
 		JPanel JP4 = new JPanel();
 		JP4.setLayout(new BorderLayout());
 		
+		// PANEL DE LA IZQ DEL PANEL DE EL CENTRO DEL PANEL DEL CENTRO DEL PANEL PRINCIPAL JP5
 		JPanel JP5 = new JPanel();
-		JP5.setLayout(new GridLayout(2,1));
+		JP5.setLayout(new FlowLayout());
 		
 		JP5.add(precio);
 		JP5.add(precio2);
 		
 		JP4.add(JP5, BorderLayout.WEST);
 
-		JP3.add(pagables);
-		JP3.add(JP4);
+		JP3.add(ScrollPagables, BorderLayout.WEST);
+		JP3.add(JP4, BorderLayout.CENTER);
 		JP1.add(JP3 , BorderLayout.CENTER);
 		
+		//PANEL DE ABAJO DEL PANEL DEL PRINCIPAL JP6
 		JPanel JP6 = new JPanel();
 		JP6.setLayout(new BorderLayout());
 		
+		//PANEL DE LA DERECHA DEL PANEL DE ABAJO DEL PANEL DEL CENTRO JP7
 		JPanel JP7 = new JPanel();
 		JP7.setLayout(new BorderLayout());
 		
@@ -87,7 +98,7 @@ public class VentanaCarrito extends JFrame{
 		
 		
 		this.setTitle("Carrito");
-		this.setSize(1200, 800);
+		this.setSize(700, 400);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(false);
 	}
