e37eaf1f4254dd3b26b4b50d83df509e812fdaf9
Oscar Perez <o.perez@opendeusto.es>
2023-01-15 18:36:14 +0100

    cacularPrecio

--- TiendaVideojuegos/src/Ventanas/calcularPrecioVentas.java
+++ TiendaVideojuegos/src/Ventanas/calcularPrecioVentas.java
@@ -14,8 +14,13 @@ import ClasesPrincipales.*;
 
 public class calcularPrecioVentas extends JFrame{
 		protected JButton atras;
+		protected JButton buscar;
+
+		
 		protected JSlider slider;
+		
 		protected JTextField textovalor;
+		
 		
 		protected DefaultListModel<Pagable> DLM;
 		protected JList<Pagable> Lista;
@@ -25,7 +30,7 @@ public class calcularPrecioVentas extends JFrame{
 		Container cp = this.getContentPane();
 		
 		atras = new JButton("Atras");
-		
+		buscar = new JButton("Buscar");
 		
 		
 		slider = new JSlider(0,1000,500);
@@ -42,15 +47,34 @@ public class calcularPrecioVentas extends JFrame{
 		textovalor.setFont(new Font("Serif", Font.ITALIC, 12));
 		textovalor.setEditable(false);
 		
+		DLM = new DefaultListModel<>();
+		Lista = new JList<>(DLM);
+		Lista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JScrollPane ScrollPane = new JScrollPane(Lista);
+		
 	
+		//el panel principal de la ventana
+		JPanel JP1 = new JPanel();
 		
-		JPanel JP = new JPanel();
-		JP.setLayout(new BorderLayout());
-		JP.add(atras, BorderLayout.NORTH);
-		JP.add(slider, BorderLayout.CENTER);
-		JP.add(textovalor, BorderLayout.SOUTH);
+		JP1.setLayout(new BorderLayout());
 		
-		cp.add(JP);
+		
+		JPanel JP2 = new JPanel();
+		JP2.setLayout(new BorderLayout());
+		
+		JP2.add(atras, BorderLayout.NORTH);
+		JP2.add(slider, BorderLayout.CENTER);
+		JP2.add(textovalor, BorderLayout.SOUTH);
+		JP2.add(buscar,BorderLayout.EAST);
+		
+		JPanel JP3 = new JPanel();
+		JP3.setLayout(new FlowLayout());
+		JP3.add(ScrollPane);
+		
+		
+		JP1.add(JP2, BorderLayout.WEST);
+		JP1.add(JP3, BorderLayout.CENTER);
+		cp.add(JP1);
 		
 		atras.addActionListener(new ActionListener() {
 			
@@ -71,6 +95,15 @@ public class calcularPrecioVentas extends JFrame{
 			}
 		});
 		
+		buscar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 		
 		
 		this.setVisible(false);
@@ -78,5 +111,38 @@ public class calcularPrecioVentas extends JFrame{
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("Calcular Precio Ventas");
 		this.setResizable(false);
+	}
+	
+	public ArrayList<ArrayList<Producto>> PosiblesComprasPorPrecio (double precioFinal, double PrecioActual, 
+			ArrayList<Producto> productos, ArrayList<ArrayList<Producto>> productosFinal) {
+		
+		if (PrecioActual > precioFinal) {
+			productosFinal.remove(productosFinal.size()-1);
+			return productosFinal;
+		} else {
+			
+			for (Producto producto : productos) {
+				if (producto.getTp() == TipoProducto.VIDEOJUEGO) {
+					Videojuego v = (Videojuego) producto;
+					
+					Double pA = PrecioActual + v.getPrecio();
+					
+				
+					
+					
+					
+
+				} else if (producto.getTp() == TipoProducto.CONSOLA) {
+					
+				} else {
+					
+				}
+				
+			}
+			
+		}
+		
+
+	return null;	
 	}
 }
