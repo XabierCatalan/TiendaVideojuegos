de0e495abba6439df36cedecc4585fd3a73da86a
Oscar Perez <o.perez@opendeusto.es>
2023-01-15 14:11:44 +0100

    calcularprecioventas slider y texto

--- TiendaVideojuegos/src/Ventanas/calcularPrecioVentas.java
+++ TiendaVideojuegos/src/Ventanas/calcularPrecioVentas.java
@@ -7,12 +7,15 @@ import java.util.Date;
 import java.util.Iterator;
 
 import javax.swing.*;
-
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import ClasesPrincipales.*;
 
 public class calcularPrecioVentas extends JFrame{
 		protected JButton atras;
+		protected JSlider slider;
+		protected JTextField textovalor;
 	
 	public calcularPrecioVentas() {
 		
@@ -20,8 +23,29 @@ public class calcularPrecioVentas extends JFrame{
 		
 		atras = new JButton("Atras");
 		
+		
+		
+		slider = new JSlider(0,1000,500);
+		slider.setMajorTickSpacing(200);
+		slider.setMinorTickSpacing(50);
+		
+		slider.setPaintTicks(true);
+		
+		slider.setFont(new Font("Serif", Font.ITALIC, 12));
+		
+		slider.setPaintLabels(true);
+		
+		textovalor = new JTextField(String.valueOf(slider.getValue()));
+		textovalor.setFont(new Font("Serif", Font.ITALIC, 12));
+		textovalor.setEditable(false);
+		
+	
+		
 		JPanel JP = new JPanel();
-		JP.add(atras);
+		JP.setLayout(new BorderLayout());
+		JP.add(atras, BorderLayout.NORTH);
+		JP.add(slider, BorderLayout.CENTER);
+		JP.add(textovalor, BorderLayout.SOUTH);
 		
 		cp.add(JP);
 		
@@ -34,6 +58,17 @@ public class calcularPrecioVentas extends JFrame{
 				
 			}
 		});
+		
+		slider.addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				textovalor.setText(String.valueOf(slider.getValue()));
+				
+			}
+		});
+		
+		
 		
 		this.setVisible(false);
 		this.setSize(800, 400);
